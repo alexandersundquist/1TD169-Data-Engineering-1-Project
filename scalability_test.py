c5bf5b977835871e7ec0129811e1778c555705ec
vfe01 <villeelings@gmail.com>
2025-03-20 15:37:36 +0100

    Fix to scalability test

--- scalability_test.py
+++ scalability_test.py
@@ -4,15 +4,16 @@ from interface import top_songs
 PROPORTIONS_OF_DATASET_TO_TEST = [0.125, 0.25, 0.5, 0.75, 1]
 
 for proportion in PROPORTIONS_OF_DATASET_TO_TEST:
-    print("proportion=",proportion)
+    print("!proportion=",proportion)
     
     start_time = time.time()
     df, spark_session = create_processed_data_df(proportion)
     print("#create df done")
     
-    top_songs(df, "093cb74eb3c517c5179ae24caf0ebec51b24d2a2", 1)
+    top_songs(df, "093cb74eb3c517c5179ae24caf0ebec51b24d2a2", 1).show()
     end_time = time.time()
     print("#top songe done")
     
-    print("time_elapsed=",end_time-start_time)
+    print("!time_elapsed=",end_time-start_time)
     
+    spark_session.stop()
